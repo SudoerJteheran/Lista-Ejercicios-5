66ac128ebdf5ee02e53667cb82f0c2fc8a91a540
Juan Manuel Teherán Machado <79525668+SudoerJteheran@users.noreply.github.com>
2023-06-05 14:29:13 -0500

    Subidita la base

---
+++ Ejercicio_8_2/src/main/java/ejercicio_8_2/ejercicio_8_2.java
@@ -0,0 +1,7 @@
+package ejercicio_8_2;
+
+public class ejercicio_8_2 {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ Ejercicio_8_3/src/main/java/Ejercicio_8_3/ejercicio_8_3.java
@@ -0,0 +1,7 @@
+package Ejercicio_8_3;
+
+public class ejercicio_8_3 {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
