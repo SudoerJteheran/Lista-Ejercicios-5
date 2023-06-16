45d409b364637095cc39bde2fbf2ed8413e2148d
Juan Manuel Teherán Machado <79525668+SudoerJteheran@users.noreply.github.com>
2023-06-16 16:52:55 +0000

    Se dejan listas las importaciones para las interfaces gráficas

---
+++ Ejercicio_8_3/src/main/java/Ejercicio_8_3/VentanaEsfera.java
@@ -0,0 +1,7 @@
+package Ejercicio_8_3;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
---
+++ Ejercicio_8_3/src/main/java/Ejercicio_8_3/VentanaPrincipal.java
@@ -0,0 +1,8 @@
+package Ejercicio_8_3;
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+// Interfaz grafica para el menu que permite seleccionar las figuras geométricas.
