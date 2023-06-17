ee261251c5265a841d7686cf56f34084ebc5fb68
Jteheranm <jteheranm@unal.edu.co>
2023-06-17 02:05:40 -0500

    Agregado TODO

--- Ejercicio_8_3/src/main/java/Ejercicio_8_3/Piramide.java
+++ Ejercicio_8_3/src/main/java/Ejercicio_8_3/Piramide.java
@@ -15,13 +15,12 @@ public class Piramide extends FiguraGeometrica{
     }
 
     //Metodos para calcular atributos
-
+    //TODO Revisar las formulas cuidadosamente
     public void calcularVolumen(){
         this.volumen = (altura*base)/3;
     }
 
     public void calcularSuperficie(){
-        //TODO Revisar formula
         this.superficie = (base*apotema)/2+Math.pow(base, 2);
     }
 
